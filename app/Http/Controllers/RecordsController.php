902e60441bcc30ce8bf44b0ec5ddb7ff57b415e5
Kooroshh <mr.oplus@yahoo.fr>
2021-02-05 02:33:09 +0330

    bug:)

--- app/Http/Controllers/RecordsController.php
+++ app/Http/Controllers/RecordsController.php
@@ -19,7 +19,7 @@ class RecordsController extends Controller
         $request->validate(['name' => "required"]);
         $result = $zone->records()->where('name',$request->name)->first();
         if($result){
-            return view('app/record/create')->withError(['name'=> 'Record already exists']);
+            return view('app/record/create')->with(['zone'=>$zone])->withError(['name'=> 'Record already exists']);
         }
         $record =  $zone->records()->create($request->all());
         return redirect()->route('show-zone-record-entries',$record->id);
