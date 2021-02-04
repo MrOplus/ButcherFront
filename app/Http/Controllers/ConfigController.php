34a2d276251072b811adf1c786bbd97ddf9e4bea
Kooroshh <mr.oplus@yahoo.fr>
2021-02-05 03:24:35 +0330

    fix config generator

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -11,18 +11,21 @@ class ConfigController extends Controller
     function apply(Request $request) {
         $structure = [] ;
         $zones = Zone::all();
-        foreach($zones as $zone){
-            $zone_array = $zone->toArray();
-            $zone_array['records'] = [];
+        foreach ($zones as $zone){
+            $zone_shadow = $zone->toArray();
+            $zone_shadow['records'] = [] ;
             foreach ($zone->records as $record){
-                $temp = $record->toArray();
-                $temp['entries'] = null ;
-                foreach ($record->entries()->orderBy('order')->get() as $entry){
-                    $temp['entries'][$entry->type][] = $entry->toArray();
-                    $zone_array['records'][] = $temp;
+                $record_shadow  = $record->toArray();
+                $record_shadow['entries'] = [] ;
+                foreach ($record->entries as $entry){
+                    $entry_shadow = $entry->toArray();
+                    if(!isset($record_shadow['entries'][$entry->type]))
+                        $record_shadow['entries'][$entry->type] = [] ;
+                    $record_shadow['entries'][$entry->type][] = $entry_shadow;
                 }
+                $zone_shadow['records'][] = $record_shadow;
             }
-            $structure[] = $zone_array;
+            $structure[] = $zone_shadow;
         }
         Storage::put('config.json',json_encode($structure));
         return view('app/config/apply')->with(['message'=>'Applying Configurations ... it may takes up to 1 minuets depends on your dataset size']);
