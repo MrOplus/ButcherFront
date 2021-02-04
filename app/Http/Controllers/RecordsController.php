f67f6972c464fb1bfbe2680723d8972f94486989
Kooroshh <mr.oplus@yahoo.fr>
2021-02-05 01:56:57 +0330

    fix validation

--- app/Http/Controllers/RecordsController.php
+++ app/Http/Controllers/RecordsController.php
@@ -16,7 +16,11 @@ class RecordsController extends Controller
     }
     function doAddRecord(Request  $request,$zone_id){
         $zone = auth()->user()->zones()->findOrFail($zone_id);
-        $request->validate(['name' => "required|unique:zone_records,name,zone_id"]);
+        $request->validate(['name' => "required"]);
+        $result = $zone->records()->where('name',$request->name)->first();
+        if($result){
+            return view('app/record/create')->withError(['name'=> 'Record already exists']);
+        }
         $record =  $zone->records()->create($request->all());
         return redirect()->route('show-zone-record-entries',$record->id);
     }
